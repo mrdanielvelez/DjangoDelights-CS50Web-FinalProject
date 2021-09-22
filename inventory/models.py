321d6aa7808a9078bbb3e1a9affb1beef5febd2c
Daniel Velez <mrdaniel8747@gmail.com>
2021-09-21 21:19:58 -0400

    Configure models

--- inventory/models.py
+++ inventory/models.py
@@ -8,28 +8,28 @@ class User(AbstractUser):
 class Ingredient(models.Model):
     name = models.CharField(max_length=25)
     quantity = models.FloatField(validators=[MinValueValidator(0)])
-    unit = models.CharField(max_length=10, default="lbs")
+    unit = models.CharField(max_length=10, default="pounds")
     unit_price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return f"{self.name} — {self.quantity} {self.unit}. available — ${self.unit_price} per unit"
+        return f"{self.name} — {self.quantity} {self.unit} available — ${self.unit_price} per unit"
 
 class MenuItem(models.Model):
-    item = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="items")
+    name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     recipe_image = models.URLField(blank=True, null=True)
     recipe_link = models.URLField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.item} — ${self.price}"
+        return f"{self.name} (${self.price})"
 
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name="requirements")
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipes")
     quantity = models.FloatField(validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f"{self.quantity} {self.ingredient.unit}. of {self.ingredient} required for {self.menu_item}"
+        return f"{self.quantity} {self.ingredient.unit} of {self.ingredient.name} — {self.menu_item.name}"
 
 class Purchase(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="purchases")
